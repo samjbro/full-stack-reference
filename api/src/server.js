ac84f3bb901abe566206e42eb9b27b4e723208ee
Sam Broughton <sambroughton@sams-mbp.home>
2019-02-24 17:24:56 +0000

    sets up apollo client & server, and fixes dev-server to work with SPA routing

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { importSchema } from 'graphql-import'
+
+import resolvers from './resolvers'
 
 const app = express()
+
+const server = new ApolloServer({
+  typeDefs: importSchema('./schema.graphql'),
+  resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
 
 export { app as default }
